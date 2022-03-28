6cb811c55251dee060d49ed06d1b055410a9b31a
Dylan097 <DylanAldridge7@gmail.com>
2022-03-28 18:41:48 +0000

    Add code to show what option the player has selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
+// Listener for clicking on the rules button
+
 document.getElementById('rules-button').addEventListener('click', function () {
     let rules = document.getElementById('rules');
     rules.style.display = 'block';
 })
 
+// Listener for clicking on the 'x' button when rules is open
+
 document.getElementById('close').addEventListener('click', function () {
     let rules = document.getElementById('rules')
     rules.style.display = 'none';
 })
+
+let options = document.getElementsByClassName('box');
+
+// Adding the selected class to the option selected, and removing it from the last one
+
+function selected() {
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].classList.contains('selected')) {
+            options[i].classList.remove('selected');
+        }
+    }
+    this.classList.add('selected');
+}
+
+// Looping through each box class and adding an event listener to them
+
+for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', selected);
+}
