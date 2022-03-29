3483793d417ca7f13c03c603b7a34641404b8f58
Dylan097 <DylanAldridge7@gmail.com>
2022-03-29 18:38:17 +0000

    Condense code, taking away needless repeats and placing them where it can be made shorter

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,21 +55,38 @@ function checkWinner(selection) {
     let randomNumber = Math.floor(Math.random() * 5);
     let cpu = cpuSelect[randomNumber];
     let playerChoice = document.getElementById('player-choice');
+    let cpuSelection = document.getElementById('computer-choice');
+    let rockIcon = `<i class="fa-solid fa-hand-back-fist"></i>`;
+    let paperIcon = `<i class="fa-solid fa-hand"></i>`;
+    let scissorsIcon = `<i class="fa-solid fa-hand-scissors"></i>`;
+    let lizardIcon = `<i class="fa-solid fa-hand-scissors"></i>`;
+    let spockIcon = `<i class="fa-solid fa-hand-scissors"></i>`;
+    if (cpu === 'rock') {
+        cpuSelection.innerHTML = rockIcon;
+    } else if (cpu === 'paper') {
+        cpuSelection.innerHTML = paperIcon;
+    } else if (cpu === 'scissors') {
+        cpuSelection.innerHTML = scissorsIcon;
+    } else if (cpu === 'lizard') {
+        cpuSelection.innerHTML = lizardIcon;
+    } else if (cpu === 'spock') {
+        cpuSelection.innerHTML = spockIcon;
+    }
     if (selection === 'rock') {
         rock(cpu);
-        playerChoice.innerHTML = `<i class="fa-solid fa-hand-back-fist"></i>`;
+        playerChoice.innerHTML = rockIcon;
     } else if (selection === 'paper') {
         paper(cpu);
-        playerChoice.innerHTML = `<i class="fa-solid fa-hand"></i>`
+        playerChoice.innerHTML = paperIcon;
     } else if (selection === 'scissors') {
         scissors(cpu);
-        playerChoice.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`
+        playerChoice.innerHTML = scissorsIcon;
     } else if (selection === 'lizard') {
         lizard(cpu);
-        playerChoice.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`
+        playerChoice.innerHTML = lizardIcon;
     } else if (selection === 'spock') {
         spock(cpu);
-        playerChoice.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`
+        playerChoice.innerHTML = spockIcon;
     } else {
         alert('Unidentified selection! Please try again');
     }
@@ -96,24 +113,12 @@ function increaseComputerScore() {
  * Rock beats lizard and scissors
  */
 function rock(computer) {
-    let cpuSelection = document.getElementById('computer-choice');
     if (computer === 'lizard' || computer === 'scissors') {
         increasePlayerScore();
-        if (computer === 'lizard') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`
-        }
     } else if (computer === 'rock') {
         alert("It's a draw!");
-        cpuSelection.innerHTML = `<i class="fa-solid fa-hand-back-fist"></i>`;
     } else {
         increaseComputerScore();
-        if (computer === 'paper') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`;
-        }
     }
 }
 
@@ -121,24 +126,12 @@ function rock(computer) {
  * Paper beats rock and Spock
  */
 function paper(computer) {
-    let cpuSelection = document.getElementById('computer-choice');
     if (computer === 'rock' || computer === 'spock') {
         increasePlayerScore();
-        if (computer === 'rock') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-back-fist"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`;
-        }
     } else if (computer === 'paper') {
-        cpuSelection.innerHTML = `<i class="fa-solid fa-hand"></i>`;
         alert("It's a draw!");
     } else {
         increaseComputerScore();
-        if (computer === 'scissors') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`;
-        }
     }
 }
 
@@ -146,24 +139,12 @@ function paper(computer) {
  * Scissors beats paper and lizard
  */
 function scissors(computer) {
-    let cpuSelection = document.getElementById('computer-choice');
     if (computer === 'paper' || computer === 'lizard') {
         increasePlayerScore();
-        if (computer === 'paper') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`;
-        }
     } else if (computer === 'scissors') {
-        cpuSelection.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`;
         alert("It's a draw!");
     } else {
         increaseComputerScore();
-        if (computer === 'rock') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-back-fist"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`;
-        }
     }
 }
 
@@ -171,24 +152,12 @@ function scissors(computer) {
  * Lizard beats Spock and paper
  */
 function lizard(computer) {
-    let cpuSelection = document.getElementById('computer-choice');
     if (computer === 'paper' || computer === 'spock') {
         increasePlayerScore();
-        if (computer === 'paper') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`;
-        }
     } else if (computer === 'lizard') {
-        cpuSelection.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`;
         alert("It's a draw!");
     } else {
         increaseComputerScore();
-        if (computer === 'rock') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-back-fist"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`;
-        }
     }
 }
 
@@ -196,23 +165,11 @@ function lizard(computer) {
  * Spock beats scissors and rock
  */
 function spock(computer) {
-    let cpuSelection = document.getElementById('computer-choice');
     if (computer === 'scissors' || computer === 'rock') {
         increasePlayerScore();
-        if (computer === 'rock') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-back-fist"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`;
-        }
     } else if (computer === 'spock') {
-        cpuSelection.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`;
         alert("It's a draw!");
     } else {
         increaseComputerScore();
-        if (computer === 'paper') {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand"></i>`;
-        } else {
-            cpuSelection.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`;
-        }
     }
 }
